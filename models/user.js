3ddabc663963dd887e60188d7a808a757051270f
ChesterKing66 <kevin13348@gmail.com>
2023-03-12 22:29:45 -0300

    creating models

---
+++ models/user.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+import database from "../database"
+
+const User = database.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+})
+
+export default User
