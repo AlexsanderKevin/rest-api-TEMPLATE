b1db15b26facf2bd982f45ec39ac29c0d91ba01c
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2023-03-15 08:44:19 -0300

    refactored user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import UserController from "../controllers/userController.js"
 
+const { getAll, getUser, createUser, updateUser, deleteUser } = UserController
+
 const router = Router()
 
-router.get( '/users', UserController.getAll )
-router.get( '/users/:id', UserController.getUser )
-router.post( '/users', UserController.createUser )
-router.put( '/users/:id', UserController.updateUser )
-router.delete( '/users/:id', UserController.deleteUser )
+router.get( '/users', getAll )
+router.get( '/users/:id', getUser )
+router.post( '/users', createUser )
+router.put( '/users/:id', updateUser )
+router.delete( '/users/:id', deleteUser )
 
 export default router
